7b4e7da2bcc575c48bb3008f983a1dda69590e2e
Mohsin0700 <muhammad786mohsin024@gmail.com>
2026-02-15 06:16:11 +0500

    JS Variables Done

---
+++ variables.js
@@ -0,0 +1,57 @@
+// console.log("Understanding var keyword");
+// console.log("Variables");
+// let userName = "Mohsin Hussain";
+// let age = 28;
+// let isMarried = true;
+// console.log(userName, age, isMarried);
+
+
+// let userName = "Mohsin Hussain";
+// console.log("Before", userName);
+// userName = "Abdul Rehman";
+// console.log("After", userName);
+
+// When we write let we can't change variable data type later on.
+
+// var userAge = 28;
+// if(userAge === 28){
+//     var newAge = userAge;
+// }
+
+// console.log(newAge);
+// Variabled declared using var are global scope.
+
+// var userAge = 30;
+// console.log(userAge);
+// Variables declared with var can be redeclare with same name. therefore we will avoid using var keyword.
+
+// ***************************************************************
+// Understanding let keyword
+
+// let userName = "Mohsin";
+// if(userName === "Mohsin"){
+//     let userAge = 30;
+// }
+
+// console.log(userage); // Error
+// Note: Variabled declared with let are blocked scope
+
+// let userName = "Mohsin"
+// console.log("User Name initial:::", userName);
+// let userName = "Arbab";
+
+// Note: variables declared with let can't be reinitialize
+
+// ***************************************************************
+// Understanding const keyword
+// ***************************************************************
+
+// Const contains all the capability of let with one extra thing is we can't reassign the value to the const  variable and we must initialize it when declaring
+// const userName = "Mohsin";
+// console.log("Variable declared with const keyword is::::::::::::::::::", userName);
+// userName = "Arbab";
+// console.log("Variable changed value is", userName);
+
+
+
+
