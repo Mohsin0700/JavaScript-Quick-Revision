b3fc82861af46772d459a47f5b129dff04a40c10
= <muhammad786mohsin024@gmail.com>
2026-02-16 11:22:25 +0500

    TypeScript Basic Variables Done

---
+++ variables.ts
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+
+let myName: string = "Mohsin Hussain";
+// myName = 28; // Will Throw an error
+
+let myAge = 28;
+// myAge = 'Mohsin Hussain' Will also through an error as if we don't define any type then it will pick assigned type automatically because the file is created with extension of .ts instead of .js....
+const isAdmin: boolean = true;
+
+console.log(myName, myAge, isAdmin);
